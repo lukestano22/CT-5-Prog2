561eb9b3826df6080e216c486d101944ba443f89
Luke Stano <lukestano22@gmail.com>
2025-07-28 00:20:32 -0500

    pushin stuff

--- src/main/java/RecursiveProduct.java
+++ src/main/java/RecursiveProduct.java
@@ -1,4 +1,31 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class RecursiveProduct {
+
+    // Recursive method to calculate product
+    public static int getProduct(int[] numbers, int index) {
+        // Base case
+        if (index == numbers.length - 1) {
+            return numbers[index];
+        }
+        // Recursive case
+        return numbers[index] * getProduct(numbers, index + 1);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] numbers = new int[5];
+
+        System.out.println("Enter 5 numbers:");
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print("Number " + (i + 1) + ": ");
+            numbers[i] = scanner.nextInt();
+        }
+
+        int product = getProduct(numbers, 0);
+        System.out.println("The product of the five numbers is: " + product);
+
+        scanner.close();
+    }
 }
